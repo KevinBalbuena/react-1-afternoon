8dcbf6bfada23d65194a34ee9f46d2ab5407202b
Kevin Balbuena <kbalbuena5@yahoo.com>
2019-09-04 20:55:49 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import TopicBrower from "./components/TopicBrowser/TopicBrowser";
 
 class App extends Component {
   render() {
-    
+    return (
+      <div>
+        <TopicBrower />
+      </div>
+    );
   }
 }
 
